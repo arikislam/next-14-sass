4af17dac7c9315d6c837813eef08a33b675dcbe5
S M Ariq Islam <arikislam321@gmail.com>
2024-03-15 00:55:48 +0600

    Mobile navigation is done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
-import {UserButton} from '@clerk/nextjs';
-
 const Home = () => {
   return <div>
     <h1>Home</h1>
     <p>Welcome to the home page!</p>
-    <UserButton afterSignOutUrl='/' />
+  
   </div>;
 };
 
